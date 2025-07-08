50f2d67e5213d9aad54a9398fe134a003c3238f4
Tyler <jakarinp1999@gmail.com>
2025-07-08 14:00:14 +0700

    CRUD API

--- backend/main.py
+++ backend/main.py
@@ -44,8 +44,60 @@ def get_session():
     with Session(engine) as session:
         yield session
 
+SessionDep = Annotated[Session, Depends(get_session)]
 app = FastAPI()
 
 @app.on_event("startup")
 def on_startup():
     create_db_and_tables()
+
+
+@app.get("/")
+def root():
+    return {"message": "Books CRUD API with SQLModel"}
+
+@app.post("/books/", response_model=BookResponse)
+def create_book(book: BookCreate, session: SessionDep):
+        db_book = Book.model_validate(book)
+        session.add(db_book)
+        session.commit()
+        session.refresh(db_book)
+        return db_book
+    
+
+@app.get("/books/", response_model=list[BookResponse])
+def read_books(
+    session: SessionDep,
+    offset: int = Query(default=0, ge=0),
+    limit: Annotated[int, Query(le=100)] = 100,
+):
+    books = session.exec(select(Book).offset(offset).limit(limit)).all()
+    return books
+
+@app.get("/books/{book_id}", response_model=BookResponse)
+def read_book(book_id: int, session: SessionDep):
+    book = session.get(Book, book_id)
+    if not book:
+        raise HTTPException(status_code=404, detail="Book not found")
+    return book
+
+@app.patch("/books/{book_id}", response_model=BookResponse)
+def update_book(book_id: int, book: BookUpdate, session: SessionDep):
+    book_db = session.get(Book, book_id)
+    if not book_db:
+        raise HTTPException(status_code=404, detail="Book not found")
+    book_data = book.model_dump(exclude_unset=True)
+    book_db.sqlmodel_update(book_data)
+    session.add(book_db)
+    session.commit()
+    session.refresh(book_db)
+    return book_db
+
+@app.delete("/books/{book_id}")
+def delete_book(book_id: int, session: SessionDep):
+    book_db = session.get(Book, book_id)
+    if not book_db:
+        raise HTTPException(status_code=404, detail="Book not found")
+    session.delete(book_db)
+    session.commit()
+    return {"ok": True}
